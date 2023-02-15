4777d463e81394aa08bcb4db8245461cc1161a7c
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-15 15:27:26 -0600

    retrying api

--- amplify/backend/function/fulltestapifunction/src/index.py
+++ amplify/backend/function/fulltestapifunction/src/index.py
@@ -1,26 +1,15 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import json
-import boto3
-from boto3.dynamodb.conditions import Key, Attr
-from pydantic import BaseModel
 
-
-app = FastAPI()
-dynamodb = boto3.resource('dynamodb', region_name="us-east-2",
-         aws_access_key_id="AKIAX3FHVXYSKFFYPGE7",
-         aws_secret_access_key= "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")
-table = dynamodb.Table('fulltestdb-staging')
-
-def lambda_handler(event, contect):
-    response = table.get_item(
-        Key={
-            'PK': 'COMP#0',
-            'SK': 'COMP#0'
-        }
-    )
-    print(response)
-    return {
-        'statusCode': 200,
-        'body': response
-    }
+def handler(event, context):
+  print('received event:')
+  print(event)
+  
+  return {
+      'statusCode': 200,
+      'headers': {
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+      },
+      'body': json.dumps('Hello from your new Amplify Python lambda!')
+  }
