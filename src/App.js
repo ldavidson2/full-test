0dc5cb443de0b8f15e27f708d0ccb013d64c4a67
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-15 14:44:47 -0600

    Revert "rebuild"
    This reverts commit 068616f6d4ec2a97e9733a1fcb654ca819aee35b.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Message from "./components/message";
 import Login from "./components/login";
 import { Amplify } from 'aws-amplify';
 
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
