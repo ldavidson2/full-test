b9979bf24a27f4c352b91a7e5dfce741b9f7aff9
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-15 14:44:51 -0600

    Revert "testing login"
    This reverts commit 5df81a059f135c1c0768090de72240a2dd9e6453.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Message from "./components/message";
-import Login from "./components/login";
-import { Amplify } from 'aws-amplify';
-
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Message />
     </div>
   );
 }
