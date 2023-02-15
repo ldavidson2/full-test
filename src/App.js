b78e3ee2c7570327da492c57f8533c34dc5b48ea
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-15 15:54:56 -0600

    rebuild

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   const [input, setInput] = useState("");
   const [users, setUsers] = useState([]);
 
-  //Function to fetch from our backend and update customers arra
+  //Function to fetch from our backend and update customers arr
   function getUser(e) {
     let PK = e.input;
     API.get(myAPI, path, {})
