1bb616053147e8fe98b8a892292be3d0404a3f3b
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-15 13:23:54 -0600

    added database

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "storage": {
+    "fulltestdb": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
